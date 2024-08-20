1df9bee4ff22501905f31f12e6f6a898b1be4c69
Clifton Teo <153004669+cliftonteo@users.noreply.github.com>
2024-08-20 14:53:49 +0800

    Update asr.py

--- whisperx/asr.py
+++ whisperx/asr.py
@@ -84,10 +84,6 @@ def load_model(whisper_arch,
         "prepend_punctuations": "\"'“¿([{-",
         "append_punctuations": "\"'.。,，!！?？:：”)]}、",
         "suppress_numerals": False,
-        "max_new_tokens": None,
-        "clip_timestamps": None,
-        "hallucination_silence_threshold": None,
-        "hotwords": None,
     }
 
     if asr_options is not None:
